7771cd4e487291d6d20e0b4a13876308afcf2065
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-03 16:55:32 +0100

    inizio jquery, aggiunto la funzione per recupare l'enter da tastiera

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//genero l'evento per attivare il tasto enter da tastiera
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+      //  alert('sono vivo');
+      //recupero il valore da input box scrivi messaggio
+      var input = $('#rispBox input').value;
+    }
+});
