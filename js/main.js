a1779a4fe48d91ddd8105bc8995b63acca020653
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-03 17:28:24 +0100

    recuperato il valore, clonato div ed inserito nel div clonato il testo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 //genero l'evento per attivare il tasto enter da tastiera
-$(document).keypress(function(e) {
+var inputElemento = $('#rispInput');
+inputElemento.keypress(function(e) {
     if(e.which == 13) {
       //  alert('sono vivo');
       //recupero il valore da input box scrivi messaggio
-      var input = $('#rispInput').val();
-      console.log(input);
+      var messagioRecuperato = inputElemento.val();
+      console.log(messagioRecuperato);
+      //recupero il nodo per il messaggio inviato
+      // e lo clono
+      var messIniviatoClone = $('.wrapMessInviato .messaggioInviato').clone();
+      
+      console.log(messIniviatoClone);
+      //gli allego il messaggio recuperato dal utento
+      messIniviatoClone.text(messagioRecuperato);
+      console.log(messIniviatoClone)
+
+      $('#zonaChat').append(messIniviatoClone);
     }
 });
