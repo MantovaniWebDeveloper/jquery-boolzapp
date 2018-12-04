67b174214548340ac68d928239139c0c3132919b
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-04 09:52:50 +0100

    stampa del mess inviato perfettamente funzionante

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@
 var inputElemento = $('#rispInput');
 var template = $('.wrapMessInviato');
 var messInviato = $('.messaggioInviato');
+template.hide();
+
 inputElemento.keypress(function(e) {
     if(e.which == 13) {
+      //nascono i div che saranno clonati
+      template.show();
+      messInviato.hide();
       //  alert('sono vivo');
       //recupero il valore da input box scrivi messaggio
       var messagioRecuperato = inputElemento.val();
@@ -11,9 +16,9 @@ inputElemento.keypress(function(e) {
       //recupero il nodo per il messaggio inviato
       // e lo clono
      var messIniviatoClone = template.clone();
-
-     messIniviatoClone.children('.messaggioInviato').text(messagioRecuperato);
-
+     messIniviatoClone.children('.messaggioInviato').show().text(messagioRecuperato);
+     //lo stampo nella zona chat
       $('#zonaChat').append(messIniviatoClone);
+
     }
 });
