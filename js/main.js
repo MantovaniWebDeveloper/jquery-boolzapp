0b82d4b10f8ba7f13ab5e8d24631700f6a576cb5
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-04 22:14:20 +0100

    aggiunta funzionalita risposta con ok, funzionante senza bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //genero l'evento per attivare il tasto enter da tastiera
 var inputElemento = $('#rispInput');
-var template = $('.template .rigaMessaggioInviato ');
+var templateInviato = $('.template .rigaMessaggioInviato ');
+
 var clear = $('.clear');
 inputElemento.keypress(function(e) {
     if(e.which == 13) {
@@ -19,12 +20,30 @@ inputElemento.keypress(function(e) {
       console.log(messagioRecuperato);
       //recupero il nodo per il messaggio inviato
       // e lo clono
-     var messIniviatoClone = template.clone();
+     var messIniviatoClone = templateInviato.clone();
+     //stampo il valore recuperato nel figlio p e timeStamp
      messIniviatoClone.children('.messaggioInviato').show().children('.testo').show().text(messagioRecuperato);
+     messIniviatoClone.children('.messaggioInviato').show().children('.timeStamp').show().text(timeStamp);
+
      //lo stampo nella zona chat
-
       $('#zonaChat').append(messIniviatoClone);
+      //resetto l'input
+      $(this).val('');
+      //setto la risposta del destinatario
+      //con la function timeout
 
-
+      setTimeout(function () {
+        //recupero il template
+        var templateRicevuto = $('.template .rigaMessaggioRicevuto ');
+        //lo clono
+        var messRicevutoClone = templateRicevuto.clone();
+        console.log(templateRicevuto);
+        console.log(messRicevutoClone);
+        //passo ai figli i valori
+        messRicevutoClone.children('.messaggioRicevuto').show().children('.testo').show().text("ok");
+        messRicevutoClone.children('.messaggioRicevuto').show().children('.timeStamp').show().text(timeStamp);
+        //lo stampo in html
+        $('#zonaChat').append(messRicevutoClone);
+      }, 1000);
     }
 });
